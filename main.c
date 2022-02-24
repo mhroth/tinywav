f456ddacdd17ecd34515f18c0d3f98fef4ebe608
Lorenz Bucher <lorenz.bucher@gmail.com>
2022-02-24 10:12:30 +0100

    simplified main file

--- main.c
+++ main.c
@@ -18,47 +18,38 @@
 
 #include <assert.h>
 
-#define NUM_CHANNELS 2
+#define NUM_CHANNELS 1
 #define SAMPLE_RATE 48000
 #define BLOCK_SIZE 512
 
 int main(int argc, char *argv[]) {
   char* outputPath = "output.wav";
-  int totalNumSamples;
-  int framesProcessed = 0;
   
   if (argc < 2) return -1;
   const char *inputPath = argv[1];
 
-  TinyWav twReader, twWriter;
-
+  TinyWav twReader;
   tinywav_open_read(&twReader, inputPath, TW_INLINE);
   if (twReader.numChannels != NUM_CHANNELS || twReader.h.SampleRate != SAMPLE_RATE) {
       printf("Supplied test wav has wrong format - should be [%d]channels, fs=[%d]\n", NUM_CHANNELS, SAMPLE_RATE);
       return -1;
   }
-  totalNumSamples = twReader.numFramesInHeader;
   
+  TinyWav twWriter;
   tinywav_open_write(&twWriter, NUM_CHANNELS, SAMPLE_RATE, TW_FLOAT32, TW_INLINE, outputPath);
 
-  assert(totalNumSamples >= BLOCK_SIZE && "input file needs to be at least 1 block long!");
-  while (framesProcessed < totalNumSamples) {
-    int samplesRead, samplesWritten;
-    int effectiveBlockSize = BLOCK_SIZE;
+  int totalNumSamples = twReader.numFramesInHeader;
+  int samplesProcessed = 0;
+  while (samplesProcessed < totalNumSamples) {
     float buffer[NUM_CHANNELS * BLOCK_SIZE];
     
-    samplesRead = tinywav_read_f(&twReader, buffer, BLOCK_SIZE);
+    int samplesRead = tinywav_read_f(&twReader, buffer, BLOCK_SIZE);
     assert(samplesRead > 0 && " Could not read from file!");
     
-    if (samplesRead < BLOCK_SIZE) {
-        effectiveBlockSize = samplesRead; // last block
-    }
-    
-    samplesWritten = tinywav_write_f(&twWriter, buffer, effectiveBlockSize);
+    int samplesWritten = tinywav_write_f(&twWriter, buffer, samplesRead);
     assert(samplesWritten > 0 && "Could not write to file!");
-    
-    assert(samplesRead == samplesWritten && "mismatch in samples read/written to file!");
-    framesProcessed += samplesRead * NUM_CHANNELS;
+
+    samplesProcessed += samplesRead * NUM_CHANNELS;
   }
 
   tinywav_close_read(&twReader);
